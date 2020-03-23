808dd9b0baa50636e6f2fade5a4b682dad1bb907
Qusai Fares <qusaifares@gmail.com>
2020-03-23 04:59:09 -0400

    setup bot and simple query function

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fetch = require('node-fetch');
+
+require('dotenv').config();
+
+const playerURL = 'https://gameinfo.albiononline.com/api/gameinfo/players/';
+const searchURL = 'http://gameinfo.albiononline.com/api/gameinfo/search?q=';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(client.user);
+  // Set the user presence
+  if (client.user) {
+    client.user.setPresence({
+      status: 'offline',
+      activity: {
+        name: 'catching all the egirls',
+        type: 'PLAYING'
+      }
+    });
+  }
+  console.log(client.user.presence);
+});
+
+client.on('message', async msg => {
+  if (msg.content.startsWith('~player')) {
+    const playerName = msg.content.substring(8);
+    const res = await fetch(searchURL + playerName);
+    const data = await res.json();
+    const player = data.players[0];
+    let msgToSend = '';
+    Object.keys(player).forEach(key => {
+      msgToSend += `\n ${key}: ${player[key]}`;
+      console.log(msgToSend);
+    });
+    msg.channel.send(msgToSend);
+  }
+});
+
+client.login(process.env.TOKEN);
