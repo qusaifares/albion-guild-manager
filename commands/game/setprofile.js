aad24d5328ff949a76c899681c3d615991dec0f4
Qusai Fares <qusaifares@gmail.com>
2020-03-25 05:22:28 -0400

    added logic added playerdata and setprofile commands

---
+++ commands/game/setprofile.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+const playerURL = 'https://gameinfo.albiononline.com/api/gameinfo/players/';
+const searchURL = 'http://gameinfo.albiononline.com/api/gameinfo/search?q=';
+module.exports = {
+  name: 'setprofile',
+  category: 'game',
+  description: 'Links member to their Albion Online profile.',
+  run: async (client, msg, args) => {
+    // if invalid
+    if (args.length !== 1) {
+      msg.channel.send(
+        `Please include one username. \`${process.env.PREFIX}setprofile <username>\``
+      );
+      return;
+    }
+
+    const searchRes = await fetch(`${searchURL}${args[0]}`);
+    const searchData = await searchRes.json();
+
+    if (!searchData.players.length) {
+      msg.channel.send('Player not found');
+      return;
+    }
+    const playerData = searchData.players[0];
+    let body = {
+      playerId: playerData.Id,
+      playerName: playerData.Name
+    };
+    if (playerData.GuildName === 'eGirl Collectors') {
+      body.isGuildMember = true;
+    }
+
+    const memberRes = await fetch(
+      `${process.env.SERVER_URL}/api/members/${msg.author.id}/edit`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    const memberData = await memberRes.json();
+    msg.channel.send(`Linked profile to ${memberData.playerName}`);
+  }
+};
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, RichEmbed, Collection } = require('discord.js');
+const client = new Client();
 const fetch = require('node-fetch');
+const watchedWords = require('./watched-words.json');
+const saveBadMessage = require('./functions/saveBadMessage');
+const addMember = require('./functions/addMember');
+
+client.commands = new Collection();
+client.aliases = new Collection();
 
 require('dotenv').config();
 
-client.commands = new Collection();
+const handlers = ['command'];
+
+handlers.forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+});
 
 const playerURL = 'https://gameinfo.albiononline.com/api/gameinfo/players/';
 const searchURL = 'http://gameinfo.albiononline.com/api/gameinfo/search?q=';
@@ -12,39 +22,53 @@ const guildURL = 'https://gameinfo.albiononline.com/api/gameinfo/guilds/';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log(client.user);
   // Set the user presence
   if (client.user) {
     client.user.setPresence({
-      status: 'offline',
+      status: 'online',
       activity: {
         name: 'catching all the egirls',
         type: 'PLAYING'
       }
     });
   }
-  console.log(client.user.presence);
+});
+
+client.on('guildMemberAdd', async member => {
+  addMember(member);
 });
 
 client.on('message', async msg => {
+  // check if message should be watched and save if yes
+  saveBadMessage(msg, watchedWords);
+  if (msg.content === `${process.env.PREFIX}addme`) {
+    addMember(msg.author);
+  }
   // If author is bot, return
   if (msg.author.bot) return;
   // If message isn't in a server, return
   if (!msg.guild) return;
   // If message doesn't start with prefix, return
   if (!msg.content.startsWith(process.env.PREFIX)) return;
-  if (msg.content.startsWith('~player')) {
-    const playerName = msg.content.substring(8);
-    const res = await fetch(searchURL + playerName);
-    const data = await res.json();
-    const player = data.players[0];
-    let msgToSend = '';
-    Object.keys(player).forEach(key => {
-      msgToSend += `\n ${key}: ${player[key]}`;
-      console.log(msgToSend);
-    });
-    msg.channel.send(msgToSend);
-  }
+
+  // If message.member is uncached, cache it.
+  if (!msg.member) msg.member = await msg.guild.fetchMember(msg);
+
+  const args = msg.content
+    .slice(process.env.PREFIX.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (!cmd.length) return;
+
+  // Get the command
+  let command = client.commands.get(cmd);
+  // If none is found, try to find it by alias
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+  // If a command is finally found, run the command
+  if (command) command.run(client, msg, args);
 });
 
 client.login(process.env.TOKEN);
